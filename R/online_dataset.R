a87dde3631d22a4eb8160b4c7fdc59f4a1e3fcda
nikiiny <59973282+nikiiny@users.noreply.github.com>
2020-02-13 11:28:01 +0100

    Add files via upload

--- R/online_dataset.R
+++ R/online_dataset.R
@@ -3,23 +3,30 @@
 #' Gets online dataset in csv format from 'https://vincentarelbundock.github.io/Rdatasets/datasets.html'
 #'
 #' @param URL url from 'https://vincentarelbundock.github.io/Rdatasets/datasets.html'
+#' @param dataname character with the name to save the dataset
 #'
 #' @return data frame
 #'
 #' @examples
 #' \dontrun {
-#' online_dataset('https://vincentarelbundock.github.io/Rdatasets/csv/boot/amis.csv')
+#' online_dataset('https://vincentarelbundock.github.io/Rdatasets/csv/boot/amis.csv','car_speedwarn')
 #' }
 #'
 #' @export
 #'
 
-online_dataset <- function(URL=NULL) {
-  if(is.null(URL)){
+online_dataset <- function(URL=NULL,dataname=NULL) {
+  if(is.null(URL)) {
     return(print('Please insert a valid URL argument'))
   }
+  else
+    if(is.null(dataname)) {
+      return(print('assign a name to save the dataset'))
+      }
   else {
     data <- read.csv(file=url(URL),header = TRUE,sep=',')[-1]
-  return(data)
-  }
+    assign(dataname,data,envir=.GlobalEnv)
+  return(data) }
 }
+
+
